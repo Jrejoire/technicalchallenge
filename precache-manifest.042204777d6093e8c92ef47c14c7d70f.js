181c1ff8a5f078247161db509f444cd6409ce316
jrej <jrejoire@hotmail.fr>
2019-07-31 10:17:20 +0200

    Updates

---
+++ precache-manifest.042204777d6093e8c92ef47c14c7d70f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b28bd4d8d5694988dccae48a8b00a9",
+    "url": "/technicalchallenge/index.html"
+  },
+  {
+    "revision": "58291d57cd57269588f9",
+    "url": "/technicalchallenge/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "10df2395dbfdc9c870cf",
+    "url": "/technicalchallenge/static/css/main.fe80a3d6.chunk.css"
+  },
+  {
+    "revision": "58291d57cd57269588f9",
+    "url": "/technicalchallenge/static/js/2.c7d755a6.chunk.js"
+  },
+  {
+    "revision": "10df2395dbfdc9c870cf",
+    "url": "/technicalchallenge/static/js/main.689797c2.chunk.js"
+  },
+  {
+    "revision": "f59e4bc7ea84573c7b4a",
+    "url": "/technicalchallenge/static/js/runtime~main.bbb978e9.js"
+  }
+]);
